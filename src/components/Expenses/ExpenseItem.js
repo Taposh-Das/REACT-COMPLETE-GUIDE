99de7214a24761c320452a2a45103db0069bf99c
Taposh-Das <taposhexp@gmail.com>
2021-12-01 23:41:26 -0600

    Controled vs Uncontrolled Components and Stateless vs Stateful Components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,22 +5,13 @@ import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
-  //function clickHandler() {}
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated");// this schedues the update of the title variable but doesn't update it yet. it's updated in the return statement in the jsx code and thats why console log in the next time will output the older value
-    console.log(title);
-  };
-
    return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
